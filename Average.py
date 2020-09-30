5d24057c61e045294876d62cd654c9720aa2c8f6
ryanwire123 <59776091+ryanwire123@users.noreply.github.com>
2020-09-30 17:50:15 -0400

    Add files via upload
    average program before loop and print statement

--- Average.py
+++ Average.py
@@ -1,2 +1,17 @@
-numberOfScores = input ("Please enter the number of scores you want to average: ")
-print (numberOfScores)
+numberOfScores = 0
+score = 0
+total = 0
+average = 0.0
+scoreCount = 0
+
+numberOfScores =  int(input ("Please enter the number of scores you want to average: "))
+
+#add a loop to make code repeast until scoreCount == numberOfScores
+score = int(input ("Please enter a score: "))
+total = total + score
+scoreCount = scoreCount + 1
+
+
+average = total / numberOfScores
+print (average)
+
