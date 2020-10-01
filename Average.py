0d6b080072ea9e6f1a72b828c7c8684fec52d0ee
ryanwire123 <59776091+ryanwire123@users.noreply.github.com>
2020-09-30 20:21:07 -0400

    Average with loop and print

--- Average.py
+++ Average.py
@@ -6,12 +6,13 @@ scoreCount = 0
 
 numberOfScores =  int(input ("Please enter the number of scores you want to average: "))
 
-#add a loop to make code repeast until scoreCount == numberOfScores
-score = int(input ("Please enter a score: "))
-total = total + score
-scoreCount = scoreCount + 1
+#add a loop to make code repeast until scoreCount == numberOfScores use a while loop
+while (scoreCount < numberOfScores):
+    score = int(input ("Please enter a score: "))
+    total = total + score
+    scoreCount = scoreCount + 1
 
-
+#can not concatenate string and float in same line so average was needed to be cast as a string
 average = total / numberOfScores
-print (average)
+print ("The average of the test scores is: " + str(average))
 
